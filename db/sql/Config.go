14d010f453ec86a118df8313e49dc6050a07846c
Sirpi Yugendar <sirpi.yugendar@iouring.com>
2026-04-09 20:02:56 +0530

    added config functionality to baselib

--- db/sql/Config.go
+++ db/sql/Config.go
@@ -1,9 +1,9 @@
 package sql
 
 type Config struct {
-	Host     string `yaml:"host"`
-	Port     string `yaml:"port"`
-	User     string `yaml:"user"`
-	Pass     string `yaml:"pass"`
-	Database string `yaml:"database"`
+	Host     string `yaml:"host" json:"host" xml:"host"`
+	Port     int    `yaml:"port" json:"port" xml:"port"`
+	User     string `yaml:"user" json:"user" xml:"user"`
+	Pass     string `yaml:"pass" json:"pass" xml:"pass"`
+	Database string `yaml:"database" json:"database" xml:"database"`
 }
