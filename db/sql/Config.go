28268040136ea2c7b3e3a3f0c836cdcd4ff591ad
Sirpi Yugendar <sirpi.yugendar@iouring.com>
2026-05-06 20:41:10 +0530

    reverted port number type back to string

--- db/sql/Config.go
+++ db/sql/Config.go
@@ -2,7 +2,7 @@ package sql
 
 type Config struct {
 	Host     string `yaml:"host" json:"host" xml:"host"`
-	Port     int    `yaml:"port" json:"port" xml:"port"`
+	Port     string `yaml:"port" json:"port" xml:"port"`
 	User     string `yaml:"user" json:"user" xml:"user"`
 	Pass     string `yaml:"pass" json:"pass" xml:"pass"`
 	Database string `yaml:"database" json:"database" xml:"database"`
